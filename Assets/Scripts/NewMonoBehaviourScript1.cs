ff626485b0f7d0eb344202c64625fdc070ebfe9a
Nilsnord68 <a25nilno@student.his.se>
2025-11-07 11:07:19 +0100

    code

---
+++ Assets/Scripts/NewMonoBehaviourScript1.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript1 : MonoBehaviour
+{
+    private int nummer = 5;
+    private int nummer2 = 10;
+
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
